7e943d79b65c793ad61db3e86f707ac513071041
Oliver Morrish <olmorris@ucalgary.ca>
2020-01-31 22:26:29 -0700

    GameController Script Added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //
+    public float shipSpeed;
+    public float distTravelled;
+    public int playerScore;
+    public int shipSpawnInterval;
+    public int unchewedGum;
+    public bool gumReady;
+
+    //
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
