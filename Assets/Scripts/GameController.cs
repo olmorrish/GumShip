b77da6d1da365624c1a512ce040faecd080a951a
Oliver Morrish <olmorris@ucalgary.ca>
2020-02-01 11:42:22 -0700

    Prototype Scene Added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,173 +1,123 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
-    // Speed Will be Represented From 10 to 20 to 30;
-    public float shipSpeed;
+//public class GameController : MonoBehaviour
+//{
+//    // Speed Will be Represented From 10 to 20 to 30;
+//    public float shipSpeed;
 
-    public float tierOne = 10;
-    public float tierTwo = 20;
-    public float tierThree = 30;
-    public float maxSpeed = 40;
+//    public float tierOne = 10;
+//    public float tierTwo = 20;
+//    public float tierThree = 30;
+//    public float maxSpeed = 40;
 
-    public float speedDecayRate = 5;
+//    public float speedDecayRate = 5;
 
-    // What tier we are currently in
-    public int currentTier = 1;
+//    // What tier we are currently in
+//    public int currentTier = 1;
 
-    // Controlled in PlayerController Class
-    public bool GoWasPressed;
+//    // Controlled in PlayerController Class
+//    public bool GoWasPressed;
 
-    public float distanceTravelled;
-    public int playerScore;
+//    public float distanceTravelled;
+//    public int playerScore;
 
-    // What Distance the Next Enemy Attack will be Triggered At
-    // This Distance is Randomly Generated 
-    // The Spawn of Enemies Increases as a Function of the Player Score
-    private float spawnDistance;
-    private int lowerSpawnBound;
-    private int upperSpawnBound;
+//    // What Distance the Next Enemy Attack will be Triggered At
+//    // This Distance is Randomly Generated 
+//    // The Spawn of Enemies Increases as a Function of the Player Score
+//    private float spawnDistance;
+//    private int lowerSpawnBound;
+//    private int upperSpawnBound;
 
-    public float oxygenLevel;
+//    public float oxygenLevel;
 
-    // Hole Management
-    // Hole States 
-    // C# Arrays Default to 0 for int
-    //  - 0 = OFF
-    //  - 1 = Basic Hole
-    //  - 2 = Gummy Hole
-    //  - 3 = Plugged with Gum
-    int numberOfHoles = 14;
-    int[] holes = new int[numberOfHoles];
+//    public int unchewedGum;
+//    public bool gumReady;
 
-    public bool gunReady;
+//    private bool beingAttacked;
 
-    private bool beingAttacked;
+//    //private EnemyController enemyEncounter;
 
-    private EnemyController enemyEncounter;
 
-    // Enemy Triggers
-    // Can be set to:
-    //      - ATTACK
-    //      - IDLE
-    //      - DYING
-    //      - OFF
-    public string slow1 = "OFF";
-    public string slow2 = "OFF";
-    public string slow3 = "OFF";
 
-    public string med1 = "OFF";
-    public string med2 = "OFF";
-    public string med3 = "OFF";
+//    //
 
-    public string fast1 = "OFF";
-    public string fast2 = "OFF";
-    public string fast3 = "OFF";
+//    // Start is called before the first frame update
+//    void Start() {
+//        gumReady = false;
+//        GoWasPressed = false;
+//        playerScore = 0;
+//        shipSpeed = 0;
 
-    //public int unchewedGum;
-    //public bool gumReady;
+//        updateSpawnDistance(); 
 
-    // Start is called before the first frame update
-    void Start() {
-        gumReady = false;
-        GoWasPressed = false;
-        playerScore = 0;
-        shipSpeed = 0;
+//    }
 
-        updateSpawnDistance(); 
+//    // Update is called once per frame
+//    void Update(){
 
-    }
+//        updateDistance();
 
-    // Update is called once per frame
-    void Update(){
+//        updateShipSpeed();
 
-        updateDistance();
+//        updateEnemySpawnRates();
 
-        updateShipSpeed();
+//        if (beingAttacked)
+//        {
+//            //enemyEncounter.updateEnemies();
+//        }
 
-        updateEnemySpawnRates();
+//        if (distanceTravelled < spawnDistance)
+//        {
+//            //enemyEncounter = new EnemyController;
+//        }
         
-        // This updates the enemy encounter
-        // Should set correct enemy sprites to active
-        // 
-        if (enemyEncounter != null)
-        {
-            enemyEncounter.updateEnemies();
-        }
+//    }
 
-        if ((distanceTravelled < spawnDistance) && (enemyEncounter == null))
-        {
-            enemyEncounter = new EnemyController(playerScore);
-            // TO-DO 
-        }
+////**********************************************************************************************************************
+//// Ship Movement
+////**********************************************************************************************************************
 
-        // If gun is ready it fires and enemy encounter ends
-        // A sprite will need to be played
-        // All enemy sprites should be turned off
-        if (gunReady)
-        {
-            enemyEncounter = null;
-            // TO-DO PLAY BLAST ANIMATION
-            // TO-DO TURN ENEMY SPRITES OFF
-        }
+//    void updateDistance()
+//    {
+//        distanceTravelled += shipSpeed;
+//        playerScore += (int)shipSpeed;
+//    }
 
-        generateHoleSprites();
-        
-    }
+//    // Ship accelerates 5x faster then it decelerates
+//    void updateShipSpeed()
+//    {
+//        if (GoWasPressed)
+//        {
+//            shipSpeed += 5;
+//        }
+//        else
+//        {
+//            shipSpeed -= 1;
+//        }
+//    }
 
-//**********************************************************************************************************************
-// Ship Movement
-//**********************************************************************************************************************
+////**********************************************************************************************************************
+//// Enemy Attacks
+////**********************************************************************************************************************
 
-    void updateDistance()
-    {
-        distanceTravelled += shipSpeed;
-        playerScore += (int)shipSpeed;
-    }
+//    // After an attack updateSpawnDistance determines at what distance the next attack should be triggered
+//    private void updateSpawnDistance()
+//    {
+//        float nextInterval = Random.Range(lowerSpawnBound, upperSpawnBound);
+//        spawnDistance = distanceTravelled + nextInterval;
+//    }
 
-    // Ship accelerates 5x faster then it decelerates
-    void updateShipSpeed()
-    {
-        if (GoWasPressed)
-        {
-            shipSpeed += 5;
-        }
-        else
-        {
-            shipSpeed -= 1;
-        }
-    }
+//    // How fast enemies spawn should ne a function of the player score
+//    private void updateEnemySpawnRates()
+//    {
+//        lowerSpawnBound = 80/playerScore;
+//        upperSpawnBound = 100/playerScore;
+//    }
 
-//**********************************************************************************************************************
-// Enemy Attacks
-//**********************************************************************************************************************
+////**********************************************************************************************************************
+//// Ship Status
+////**********************************************************************************************************************
 
-    // After an attack updateSpawnDistance determines at what distance the next attack should be triggered
-    private void updateSpawnDistance()
-    {
-        float nextInterval = Random.Range(lowerSpawnBound, upperSpawnBound);
-        spawnDistance = distanceTravelled + nextInterval;
-    }
-
-    // How fast enemies spawn should ne a function of the player score
-    private void updateEnemySpawnRates()
-    {
-        lowerSpawnBound = 80/playerScore;
-        upperSpawnBound = 100/playerScore;
-    }
-
-//**********************************************************************************************************************
-// Ship Status
-//**********************************************************************************************************************
-
-    void generateHoleSprites()
-    {
-        for (int i = 0; i < numOfHoles; i++)
-        {
-            // TO-DO PLAY CORRECT SPRITE BASED ON HOLE STATUSES
-        }
-    }
-
-}
+//}
