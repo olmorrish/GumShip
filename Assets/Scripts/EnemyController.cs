76d5859fcdd69d190b5ef46f57461c0cb5f85185
Stephen Dios <stephendiosdeveloper@gmail.com>
2020-02-01 10:59:14 -0700

    Fixed compiler errors with enemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -33,7 +33,9 @@ public class EnemyController : MonoBehaviour
     void setup(float distance)
     {
         this.currentDistance = distance;
-        typesOfEnemiesInSlots = (0, 0, 0);
+        typesOfEnemiesInSlots[0] = 0;
+        typesOfEnemiesInSlots[1] = 0;
+        typesOfEnemiesInSlots[2] = 0;
         numberOfHolesCreated = 0;
 
     }
@@ -45,14 +47,32 @@ public class EnemyController : MonoBehaviour
     /// Generates the enemies for the encounter, for now 1 narwhal in each slot
     /// </summary>
     void createEncounter() {
-        typesOfEnemiesInSlots = (1, 1, 1);
+        typesOfEnemiesInSlots[0] = 1;
+        typesOfEnemiesInSlots[1] = 1;
+        typesOfEnemiesInSlots[2] = 1;
     }
 
     /// <summary>
     /// Should be called by gameController once per update. This processes the enemy attacks
     /// </summary>
     public void updateAttack() {
+        //go through enemies in slots, get total enemy amt
+        int totalEnemies = 0;
 
+        for(int i = 0; i < 3; i++)
+        {
+            if (typesOfEnemiesInSlots[i] > 0)
+                totalEnemies++;
+        }
+
+        //random chance that each enemy hits, assume it is all simple narwhal for now
+        for(int i = 0; i < totalEnemies; i++)
+        {
+            int hitChance = Random.Range(1, 180);
+
+            if (hitChance < 30)
+                numberOfHolesCreated++;
+        }
     }
 
 }
