073a07844b7b3e172df15c830c0a014aa579fdc4
Joshua Novak <novakjo@shernet.sheridancollege.ca>
2020-02-02 11:13:10 -0700

    Menu dsgn done, Menu music done, Play takes you to Game scene.

---
+++ Assets/MenuControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToGameScene() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
